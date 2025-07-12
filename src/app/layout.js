0c321ae8c322634ea2b898fa7bcbef0afd254a3d
Thomasrey99 <thomas_rey1999@outlook.com>
2025-07-12 18:15:28 -0300

    change directory of components

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,5 @@
-import Footer from "./_components/footer";
-import Header from "./_components/header";
+import Footer from "../components/footer";
+import Header from "../components/header";
 import "./globals.css";
 
 export const metadata = {
